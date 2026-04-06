4ba3d50ee59768a8d37cabe5aa768c9d41eaf8c1
Hamesss <jamesboase123@gmail.com>
2026-04-06 19:19:51 -0400

    added rattlebones idle anim and other changes

--- MGS Fall Game Jam 2025/Assets/Scripts/EnemyAI/AnimStates/AnimStatesBase.cs
+++ MGS Fall Game Jam 2025/Assets/Scripts/EnemyAI/AnimStates/AnimStatesBase.cs
@@ -309,8 +309,6 @@ public class AnimStatesBase : MonoBehaviour
 
     protected void playAttackSound(EventReference sound)
     {
-        Debug.Log("is this working??");
-
         if (attackInstance.isValid())
         {
             attackInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
