46b732bbfaac6b8f5d34f791417d981b224accc8
ShadeBolt78 <shadebolt78@gmail.com>
2026-04-07 03:15:35 -0400

    lava enemy placements, size changes, currency adjustments, yadda yadda, this better work

--- MGS Fall Game Jam 2025/Assets/Scripts/EnemyAI/AnimStates/Ice/GolemiteAnimStates.cs
+++ MGS Fall Game Jam 2025/Assets/Scripts/EnemyAI/AnimStates/Ice/GolemiteAnimStates.cs
@@ -27,7 +27,7 @@ public class Golemite : AnimStatesBase
             Debug.Log($"Golemite running dist {(runToPoint-transform.position).magnitude}");
         }
 
-        if (runningToPoint && (runToPoint-transform.position).magnitude < 0.5f)
+        if (runningToPoint && (runToPoint-transform.position).magnitude < 2f)
         {
             runningToPoint = false;
             golemiteAttack();
--- MGS Fall Game Jam 2025/Assets/Scripts/EnemyAI/GolemiteAttack.cs
+++ MGS Fall Game Jam 2025/Assets/Scripts/EnemyAI/GolemiteAttack.cs
@@ -4,11 +4,13 @@ public class GolemiteAttack : MonoBehaviour
 {
     [SerializeField] GameObject lingerPrefab;
     [SerializeField] float lingerDuration;
+    [SerializeField] int damage;
 
     void OnDestroy()
     {
         GolemiteAttackLinger gal = Instantiate(lingerPrefab).GetComponent<GolemiteAttackLinger>();
         gal.transform.position = transform.position;
         gal.setLingerDuration(lingerDuration);
+        gal.setDamage(damage);
     }
 }
